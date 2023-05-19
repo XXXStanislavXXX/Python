860e80c04f74cac60f7a0e4808797e434c4321e2
Stanislav <stasperfiliyev@gmail.com>
2023-05-20 00:47:52 +0600

    hw s_7

--- семинар_7/ДЗ/HW_2.py
+++ семинар_7/ДЗ/HW_2.py
@@ -20,7 +20,7 @@
 
 """
 
-def print_operation_table(operation, num_rows=6, num_columns=6):
+def print_operation_table(operation, num_rows=int(input("enter raws ")), num_columns=int(input("enter columns "))):
     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
     for i in a:
         print(*[f"{x:>3}" for x in i])
