1b9186fe086755c76068f5a3371fe5797686bb25
Stanislav <stasperfiliyev@gmail.com>
2023-05-24 00:59:46 +0600

    s_8 hw

--- семинар_8/ДЗ/task_1.py
+++ семинар_8/ДЗ/task_1.py
@@ -8,3 +8,16 @@ def print_records(file_name: str):
         for line in data:
             print(*line.split(';'), end='')
 
+
+def input_records(file_name: str):
+    with open(file_name, 'r+', encoding='utf-8') as data:
+        record_id = 0
+        for line in data:
+            if line != '':
+                record_id = line.split(';', 1)[0]
+        print('Введите фамилию, имя, отчество, номер телефона через пробел')
+        line = f'{int(record_id) + 1};' + ';'.join(input().split()[:4]) + ';\n'
+        confirm = confirmation('добавление записи')
+        if confirm == 'y':
+            data.write(line)
+
